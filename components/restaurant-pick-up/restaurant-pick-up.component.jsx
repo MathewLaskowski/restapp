cc7e0da77df3523ae5bfd7a52e2edcb3c724e0c1
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-26 18:42:49 +0200

    add styled-components

---
+++ components/restaurant-pick-up/restaurant-pick-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RestaurantPickUp = () => (
+  <RestaurantPickUpLayout>Pick up restaurant</RestaurantPickUpLayout>
+)
+
+const RestaurantPickUpLayout = styled.div`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+`
+
+export default RestaurantPickUp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+import RestaurantPickUp from '../components/restaurant-pick-up/restaurant-pick-up.component';
+
+const HomePage = () => (
+  <>
+    <GlobalStyle />
+    <Title>Rest App</Title>
+    <RestaurantPickUp />
+  </>
+)
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: linear-gradient(93deg, rgba(82,11,36,1) 0%, rgba(156,8,8,1) 56%, rgba(213,83,0,1) 100%);
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+  }
+`
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  font-size: 6rem;
+  font-family: Arial, monospace;
+  min-width: 400px;
+`
 
 export default HomePage
